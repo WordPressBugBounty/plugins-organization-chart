d73c83bfcbf5a41ae3e347fc9fa8261a35638952
Sean Murphy <sean@iamseanmurphy.com>
2025-02-13 09:18:16 +0000

    1.5.9

--- organization-chart/library/user_library.php
+++ organization-chart/library/user_library.php
@@ -5,14 +5,14 @@ defined('ABSPATH') || exit;
 class wpda_org_chart_user_permissions_library {
 	private static $current_user;
 
-	/*############ Function for the initial information ##################*/		
-	
+	/*############ Function for the initial information ##################*/
+
 	public static function initial_information() {
 		self::$current_user = get_current_user_id();
 	}
 
-	/*############ Function for setting the ID to meta key ##################*/		
-	
+	/*############ Function for setting the ID to meta key ##################*/
+
 	public static function set_id_to_meta_key($id, $meta_key) {
 		$user_meta = self::get_all_ids($meta_key);
 		if (isset($user_meta[0]) && is_array($user_meta[0])) {
@@ -24,8 +24,8 @@ class wpda_org_chart_user_permissions_library {
 		}
 	}
 
-	/*############ Function for removing the ID from meta key ##################*/		
-	
+	/*############ Function for removing the ID from meta key ##################*/
+
 	public static function remove_id_from_meta_key($id, $meta_key) {
 		$user_meta = self::get_all_ids($meta_key);
 		$key = array_search($id, $user_meta);
@@ -47,8 +47,8 @@ class wpda_org_chart_user_permissions_library {
 		}
 	}
 
-	/*############ Function for getting all users IDs ##################*/		
-	
+	/*############ Function for getting all users IDs ##################*/
+
 	private static function get_all_user_ids() {
 		$users = get_users();
 		$user_ids = array();
@@ -58,8 +58,8 @@ class wpda_org_chart_user_permissions_library {
 		return $user_ids;
 	}
 
-	/*############ Function for getting all IDs ##################*/		
-	
+	/*############ Function for getting all IDs ##################*/
+
 	public static function get_all_ids($meta_key, $user_id = 0) {
 		if ($user_id == 0) {
 			$user_id = self::$current_user;
@@ -71,8 +71,8 @@ class wpda_org_chart_user_permissions_library {
 		return $user_meta;
 	}
 
-	/*############ Function for finding out if the current user can edit the element ##################*/		
-	
+	/*############ Function for finding out if the current user can edit the element ##################*/
+
 	public static function can_current_user_edit_element($key, $id) {
 		if (current_user_can('manage_options')) {
 			return true;
@@ -84,10 +84,10 @@ class wpda_org_chart_user_permissions_library {
 		return false;
 	}
 
-	/*############ Function for the user roles ##################*/	
-	
+	/*############ Function for the user roles ##################*/
+
 	public static function get_users() {
-		$users_role = get_option("wp_user_roles");
+		global $wpdb;
 		$users = array(
 			"manage_options" => __("Administrator", 'booking-calendar'),
 			"publish_pages" => __("Editor", 'booking-calendar'),
@@ -95,14 +95,33 @@ class wpda_org_chart_user_permissions_library {
 			"edit_posts" => __("Contributor", 'booking-calendar'),
 			"read" => __("Subscriber", 'booking-calendar')
 		);
+		$all_users = self::get_array_of_user_capabilities_and_names();
+		$users = $users + $all_users;
+		return  $users;
+	}
+
+	private static function get_array_of_user_capabilities_and_names() {
+		$users_role = wp_roles();
+		$users_role = $users_role->roles;
+		$array_of_user_capabilities_and_names = array();
+		$unwanted_capitibilities = array("level_0", "level_1", "level_2", "level_3", "level_4", "level_5", "level_6", "level_7", "level_8", "level_9", "level_10", "manage_options", "publish_pages", "publish_posts", "edit_posts", "read");
 		$user_arr = array("administrator", "editor", "author", "contributor", "subscriber");
-		if ($users_role && is_array($users_role)) {
-			foreach ($users_role as $key => $user) {
-				if (!in_array($key, $user_arr)) {
-					$users[$key] = $user["name"];
+		foreach ($users_role as $key => $value) {
+			if (in_array($key, $user_arr)) {
+				continue;
+			}
+			if (is_array($value["capabilities"])) {
+				$current_capitibilities = 'manage_options';
+				foreach ($value["capabilities"] as $key_1 => $value_1) {
+					if (in_array($key_1, $unwanted_capitibilities)) {
+						continue;
+					}
+					$current_capitibilities = $key_1;
+					break;
 				}
 			}
+			$array_of_user_capabilities_and_names[$current_capitibilities] = $value["name"];
 		}
-		return  $users;
+		return $array_of_user_capabilities_and_names;
 	}
 }
